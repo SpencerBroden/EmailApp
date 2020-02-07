0f91da0abfc13002b9ca5140f14e215a605a803d
SpencerBroden <9060485+SpencerBroden@users.noreply.github.com>
2020-02-06 20:10:39 -0800

    Instance Variables

--- EmailProject/src/emailapp/Email.java
+++ EmailProject/src/emailapp/Email.java
@@ -1,5 +1,24 @@
 package emailapp;
 
 public class Email {
-
+    
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailBoxCapacity;
+    private String alternativeEmail;
+    
+    // Constructor to receive the first name and last name
+    
+    // Ask for the department
+    
+    // Generate a random password
+    
+    // Set the mailbox capacity
+    
+    // Set the alternate email
+    
+    // Change the password
+    
 }

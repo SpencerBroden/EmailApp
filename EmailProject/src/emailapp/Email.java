c53f600c95675772817dcd0513c7963ea652053f
SpencerBroden <9060485+SpencerBroden@users.noreply.github.com>
2020-02-07 16:40:18 -0800

    Set the Department Method

--- EmailProject/src/emailapp/Email.java
+++ EmailProject/src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
     
     private String firstName;
@@ -15,11 +17,27 @@ public class Email {
 	this.lastName = lastName;
 	System.out.println("Email Created: " + this.firstName + " " + this.lastName);
 	
+	// Call a method asking for the department - return the department
+	this.department = setDepartment();
+	System.out.println("Department: " + this.department);
     }
     // Ask for the department
-    
+    private String setDepartment() {
+	System.out.println("Department Codes:\n1 for Sales\n2 for Development\n3 for Accounting \n0 for None");
+	System.out.print("Enter Department: ");
+	Scanner in = new Scanner(System.in);
+	int deptChoice = in.nextInt();
+	in.close();
+	switch(deptChoice) {
+	case 1: return "sales";
+	case 2: return "dev";
+	case 3: return "acct";
+	case 0: return "";
+	default: return "";
+	}
+    }
     // Generate a random password
-    
+
     // Set the mailbox capacity
     
     // Set the alternate email
